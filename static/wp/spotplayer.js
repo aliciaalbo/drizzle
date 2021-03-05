ebe80a88071b06a0583c17846a59526d73796691
Alicia Albo <aliciaalbo@gmail.com>
2021-03-05 19:44:50 +0000

    add first half play/pause toggling

--- static/wp/spotplayer.js
+++ static/wp/spotplayer.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import useStickyState from "./useStickyState";
 
 // props: access_token, webplayer, playerstate
 function SpotPlayer(props) {
+    const [playbackToggle, setPlaybackToggle] = useStickyState('no', "playbackToggle");
+
     console.log('spotplayer:',props);
     const deviceId = props.webplayer.player._options.id;
     function play(e) {
         e.preventDefault();
         const body = JSON.stringify({ uris: props.playstate.uris, offset: { position: props.playstate.offset } });
+
+        setPlaybackToggle('yes');
+
         return fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
             method: 'PUT',
             body: body,
@@ -15,10 +21,33 @@ function SpotPlayer(props) {
                 'Authorization': `Bearer ${props.access_token}`
             },
         });
+    }	
+    function pause(e) {
+        e.preventDefault();
+        props.webplayer.player.togglePlay().then(() => {
+            console.log('Toggled playback!');
+          });
     }
+    function prev(e) {
+        e.preventDefault();
+        props.webplayer.player.previousTrack().then(() => {
+        console.log('Set to previous track!');
+      });
+    }
+    function next(e) {
+    e.preventDefault();
+    props.webplayer.player.nextTrack().then(() => {
+    console.log('Set to previous track!');
+        });
+    }   
+
     return (
         <section>
-            <button onClick={e => { play(e) }}>Play</button>
+            {playbackToggle === 'yes' ? <button onClick={e => { pause(e) }}>play/Pause</button> : <button onClick={e => { play(e) }}>Play</button>}
+            
+            
+            <button onClick={e => { prev(e) }}>Previous</button>
+            <button onClick={e => { next(e) }}>Next</button>
         </section>
     )
 }
