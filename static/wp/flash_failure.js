370f2c125796bff590d7041b9b14422e7b91f66e
Alicia Albo <aliciaalbo@gmail.com>
2021-03-11 00:27:51 +0000

    dark buttons, dark variant to make active outline look better

--- static/wp/flash_failure.js
+++ static/wp/flash_failure.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import FlashMessage from 'react-flash-message';
-import Alert from 'react-bootstrap/Alert'
+import Alert from 'react-bootstrap/Alert';
  
 
 function Failure(){
     return(
     <FlashMessage duration={5000} persistOnHover={true}>
-    <Alert variant="dark">Something went wrong! Try logging out and loggin in again.</p>
+    <Alert variant="dark">Something went wrong! Try logging out and loggin in again.</Alert>
     </FlashMessage>)
 }
 
--- static/wp/flash_success.js
+++ static/wp/flash_success.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import FlashMessage from 'react-flash-message';
+import Alert from 'react-bootstrap/Alert';
  
 
 function Success(){
     return(
     <FlashMessage duration={5000} persistOnHover={true}>
-    <p class="text-white">Success! Go to Spotify to view your playlist</p>
+    <Alert variant="dark"> class="text-white">Success! Go to Spotify to view your playlist</Alert>
     </FlashMessage>)
 }
 
